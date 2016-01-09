dd6fa6bd95b724181e1474f8a20c5e4ccb00a8a6
Steven Buchko <stevenbuchko@gmail.com>
2016-01-08 23:37:15 -0500

    Error messages for Sign Up

--- app/src/main/java/com/stevenbuchko/hereapp/SignUpActivity.java
+++ app/src/main/java/com/stevenbuchko/hereapp/SignUpActivity.java
@@ -1,16 +1,71 @@
 package com.stevenbuchko.hereapp;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mUsername = (EditText) findViewById(R.id.usernameSignUp);
+        mPassword = (EditText) findViewById(R.id.passwordSignUp);
+        mEmail = (EditText) findViewById(R.id.emailSignUp);
+        mSignUpButton = (Button) findViewById(R.id.signUpButton);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
+                username = username.trim();
+                password = password.trim();
+                email = email.trim();
+
+                if (username.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.username_signup_error)
+                            .setTitle(R.string.signup_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else if (password.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.password_signup_error)
+                            .setTitle(R.string.signup_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else if (email.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.email_signup_error)
+                            .setTitle(R.string.signup_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    // create the new user
+                }
+            }
+        });
     }
 
     @Override
