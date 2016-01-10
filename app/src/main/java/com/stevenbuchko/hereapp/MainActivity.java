1dbfe99b22c149ab8088b4605be774e824c32fa4
Steven Buchko <stevenbuchko@gmail.com>
2016-01-10 09:29:49 -0500

    Remove back button from Main Activity

--- app/src/main/java/com/stevenbuchko/hereapp/MainActivity.java
+++ app/src/main/java/com/stevenbuchko/hereapp/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
         ParseUser currentUser = ParseUser.getCurrentUser();
 
         if (currentUser == null) {
